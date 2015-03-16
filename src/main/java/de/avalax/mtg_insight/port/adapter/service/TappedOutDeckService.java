d3ba3eddcb81c5e0534a92667cda3929d8c4b201
toadette <melanie.loebner@gmx.de>
2015-03-16 22:04:21 +0100

    tappedoutdeckservice added

---
+++ src/main/java/de/avalax/mtg_insight/port/adapter/service/TappedOutDeckService.java
@@ -0,0 +1,7 @@
+package de.avalax.mtg_insight.port.adapter.service;
+
+/**
+ * Created by Melanie on 15.03.2015.
+ */
+public class TappedOutDeckService {
+}
---
+++ src/test/java/de/avalax/mtg_insight/port/adapter/service/TappedOutDeckServiceTest.java
@@ -0,0 +1,33 @@
+package de.avalax.mtg_insight.port.adapter.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Melanie on 15.03.2015.
+ */
+public class TappedOutDeckServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testDeckFromDeckname() throws Exception {
+
+    }
+
+    @Test
+    public void testDecknames() throws Exception {
+
+    }
+}

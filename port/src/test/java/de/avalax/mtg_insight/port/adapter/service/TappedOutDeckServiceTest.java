643a46b5a50d6466c6aa90004a8d05fc4933d279
toadette <melanie.loebner@gmx.de>
2015-03-17 21:35:41 +0100

    modified test

--- port/src/test/java/de/avalax/mtg_insight/port/adapter/service/TappedOutDeckServiceTest.java
+++ port/src/test/java/de/avalax/mtg_insight/port/adapter/service/TappedOutDeckServiceTest.java
@@ -17,10 +17,11 @@ public class TappedOutDeckServiceTest {
 
     private String deckname;
     private TappedOutDeckService tappedOutDeckService;
+
     @Before
     public void setUp() throws Exception {
-        deckname="from-the-grave-to-the-cradle";
-        tappedOutDeckService=new TappedOutDeckService(deckname);
+        deckname = "from-the-grave-to-the-cradle";
+        tappedOutDeckService = new TappedOutDeckService(deckname);
     }
 
     @After
@@ -31,9 +32,14 @@ public class TappedOutDeckServiceTest {
     @Test
     public void testDeckFromDeckname() throws Exception {
         Deck deck = tappedOutDeckService.deckFromDeckname(new Deckname("from-the-grave-to-the-cradle"));
-        assertThat(deck.name().getName(),Matchers.equalTo("from-the-grave-to-the-cradle"));
-        assertThat(deck.cardsOfDeck().size(),Matchers.equalTo(30));
-        assertThat(deck.cardsOfDeck().get(0).name(),Matchers.equalTo("Boneyard Wurm"));
+        assertThat(deck.name().getName(), Matchers.equalTo("from-the-grave-to-the-cradle"));
+        assertThat(deck.cardsOfDeck().size(), Matchers.equalTo(30));
+        assertThat(deck.cardsOfDeck().get(0).name(), Matchers.equalTo("Boneyard Wurm"));
+        assertThat(deck.cardsOfDeck().get(0).count(), Matchers.equalTo(2));
+        assertThat(deck.cardsOfDeck().get(8).name(), Matchers.equalTo("Elvish Mystic"));
+        assertThat(deck.cardsOfDeck().get(8).count(), Matchers.equalTo(2));
+        assertThat(deck.cardsOfDeck().get(23).name(), Matchers.equalTo("Swamp"));
+        assertThat(deck.cardsOfDeck().get(23).count(), Matchers.equalTo(5));
     }
 
     @Test

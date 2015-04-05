ad8c15d7d757d9d421d33e24e50ac84ef0f8978f
Benjamin <github@avalax.de>
2015-04-05 22:54:01 +0200

    DeckAdapter Test

--- presentation/src/main/java/de/avalax/mtg_insight/presentation/card/DeckAdapter.java
+++ presentation/src/main/java/de/avalax/mtg_insight/presentation/card/DeckAdapter.java
@@ -4,20 +4,35 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import de.avalax.mtg_insight.application.representation.CardRepresentation;
 import de.avalax.mtg_insight.domain.model.card.Card;
 import de.avalax.mtg_insight.domain.model.deck.Deck;
 
 public class DeckAdapter extends BaseAdapter {
-    private List<Card> cards;
+    private List<CardRepresentation> cards;
     private Context context;
 
     public DeckAdapter(Context context, Deck deck) {
         this.context = context;
-        cards = deck.deck();
+        cards = new ArrayList<>();
+        SortedMap<String, CardRepresentation> groupedCards = new TreeMap<>();
+        for (Card card : deck.deck()) {
+            if (groupedCards.containsKey(card.name())) {
+                groupedCards.get(card.name()).setCountOfCard(groupedCards.get(card.name()).count() + 1);
+            } else {
+                groupedCards.put(card.name(), new CardRepresentation(card));
+            }
+        }
+        cards.addAll(groupedCards.values());
     }
 
     @Override
@@ -37,7 +52,10 @@ public class DeckAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //TODO: group same cards in constructor as CardRepresentation
-        return new CardView(context, new CardRepresentation(cards.get(position)));
+        CardView cardView = new CardView(context, cards.get(position));
+        cardView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        return cardView;
     }
 }

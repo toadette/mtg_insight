4c2edbdae38e835539760993c057f6284d26b378
Benjamin <github@avalax.de>
2015-04-05 18:39:02 +0200

    use MaterialDesign Toolbar

--- presentation/src/main/java/de/avalax/mtg_insight/presentation/card/CardStageActivity.java
+++ presentation/src/main/java/de/avalax/mtg_insight/presentation/card/CardStageActivity.java
@@ -2,6 +2,8 @@ package de.avalax.mtg_insight.presentation.card;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import de.avalax.mtg_insight.R;
 
@@ -11,10 +13,20 @@ public class CardStageActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_stage);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.card_stage_toolbar);
+        setSupportActionBar(toolbar);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("CARD_DEMO")) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new CardDemoFragment()).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_card_stage, menu);
+        return true;
     }
 }

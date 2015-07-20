f08c6763323b0addedd9400ced317b771e13f8df
Benjamin <github@avalax.de>
2015-07-20 18:36:55 +0200

    CardDrawingCalculator

--- domain/src/main/java/de/avalax/mtg_insight/domain/model/cardDrawing/CardDrawingCalculator.java
+++ domain/src/main/java/de/avalax/mtg_insight/domain/model/cardDrawing/CardDrawingCalculator.java
@@ -1,20 +1,44 @@
 package de.avalax.mtg_insight.domain.model.cardDrawing;
 
+import java.math.BigDecimal;
+
 import de.avalax.mtg_insight.domain.model.card.Card;
 import de.avalax.mtg_insight.domain.model.deck.CardCollection;
 
 public class CardDrawingCalculator {
 
-    public double cardDrawingCalculator(CardCollection cards, Card card) {
+    public double cardDrawingCalculator(int cardsToDraw, CardCollection cards, Card card) {
         if (cards.size() == 0) {
             return 0;
         }
-        double cardCount = 0;
+        int cardCount = cardCount(card, cards);
+        double drawPercentage = 100.0 - (multiplicator(1, cardsToDraw, cards, cardCount)) * 100.0;
+        return roundHalfUp(drawPercentage);
+    }
+
+    private int cardCount(Card card, CardCollection cards) {
+        int cardsInCollection = 0;
         for (Card c : cards) {
             if (c.equals(card)) {
-                cardCount++;
+                cardsInCollection++;
             }
         }
-        return 100.0 - (((cards.size() - cardCount) / cards.size()) * 100.0);
+        return cardsInCollection;
+    }
+
+    private double multiplicator(double percentage, double cardsToDraw, CardCollection cards, double cardCountInCollection) {
+        double cardsLeftToDraw = cardsToDraw - 1;
+        double newPercentage = percentage * (cards.size() - cardCountInCollection - cardsLeftToDraw) / (cards.size() - cardsLeftToDraw);
+        if (cardsLeftToDraw == 0) {
+            return newPercentage;
+        } else {
+            return multiplicator(newPercentage, cardsLeftToDraw, cards, cardCountInCollection);
+        }
+    }
+
+    private double roundHalfUp(double drawPercentage) {
+        BigDecimal bd = new BigDecimal(Double.toString(drawPercentage));
+        bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
+        return bd.doubleValue();
     }
 }
--- domain/src/test/java/de/avalax/mtg_insight/domain/model/cardDrawing/CardDrawingCalculatorTest.java
+++ domain/src/test/java/de/avalax/mtg_insight/domain/model/cardDrawing/CardDrawingCalculatorTest.java
@@ -3,21 +3,46 @@ package de.avalax.mtg_insight.domain.model.cardDrawing;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.avalax.mtg_insight.domain.model.card.Card;
 import de.avalax.mtg_insight.domain.model.card.CardBuilder;
 import de.avalax.mtg_insight.domain.model.deck.CardCollection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardDrawingCalculatorTest {
-    private void assertDrawPercentage(double expectedDrawPercentage, String cardName, CardCollection cards) {
-        double drawPercentage = cardDrawingCalculator.cardDrawingCalculator(cards, new CardBuilder(cardName).build());
+    private void assertDrawPercentage(int cardsDrawnBefore, double expectedDrawPercentage, String cardName, CardCollection cards) {
+        double drawPercentage = cardDrawingCalculator.cardDrawingCalculator(cardsDrawnBefore, cards, new CardBuilder(cardName).build());
 
         assertThat(drawPercentage).isEqualTo(expectedDrawPercentage);
+    }
+
+    private void assertDrawPercentage(double expectedDrawPercentage, String cardName, CardCollection cards) {
+        double drawPercentage = cardDrawingCalculator.cardDrawingCalculator(1, cards, new CardBuilder(cardName).build());
+
+        assertThat(drawPercentage).isEqualTo(expectedDrawPercentage);
+    }
+
+    private CardCollection cards(CardCollection... cards) {
+        CardCollection cardCollection = new CardCollection();
+        for (CardCollection cardsColl : cards) {
+            for (Card card : cardsColl) {
+                cardCollection.add(card);
+            }
+        }
+        return cardCollection;
     }
 
     private CardCollection cards(String... cards) {
         CardCollection cardCollection = new CardCollection();
         for (String card : cards) {
+            cardCollection.add(new CardBuilder(card).build());
+        }
+        return cardCollection;
+    }
+
+    private CardCollection cards(String card, int times) {
+        CardCollection cardCollection = new CardCollection();
+        for (int i = 0; i < times; i++) {
             cardCollection.add(new CardBuilder(card).build());
         }
         return cardCollection;
@@ -32,10 +57,24 @@ public class CardDrawingCalculatorTest {
 
     @Test
     public void singleCards_shouldReturnDrawingPercentage() throws Exception {
-        assertDrawPercentage(0.00, "unknownCardInEmptyDeck", cards());
+        assertDrawPercentage(0.00, "unknownCardInEmptyDeck", new CardCollection());
         assertDrawPercentage(0.00, "unknownCard", cards("aCard", "anotherCard"));
         assertDrawPercentage(100.00, "singleCardInDeck", cards("singleCardInDeck"));
-        assertDrawPercentage(100.00, "onlyCardIdDeck", cards("onlyCardIdDeck", "onlyCardIdDeck"));
+    }
+
+    @Test
+    public void multipleCards_shouldReturnDrawingPercentage() throws Exception {
+        assertDrawPercentage(100.00, "onlyCardIdDeck", cards("onlyCardIdDeck", 2));
         assertDrawPercentage(50.00, "oneOfTwoCardsInDeck", cards("oneOfTwoCardsInDeck", "anotherCard"));
+        assertDrawPercentage(10.00, "tenPercentOfDeck", cards(cards("tenPercentOfDeck", 6), cards("anotherCard", 54)));
+        assertDrawPercentage(50.00, "fiftyPercentOfDeck", cards(cards("fiftyPercentOfDeck", 30), cards("anotherCard", 30)));
+    }
+
+    @Test
+    public void percentageAfterMultipleTurns_shouldReturnDrawingPercentage() throws Exception {
+        assertDrawPercentage(2, 100.00, "oneOfTwoCardsInDeck", cards("oneOfTwoCardsInDeck", "anotherCard"));
+        assertDrawPercentage(3, 60.00, "oneOfTwoCardsInDeck", cards(cards("oneOfTwoCardsInDeck", 1), cards("anotherCard", 4)));
+        assertDrawPercentage(7, 39.95, "playset", cards(cards("playset", 4), cards("anotherCard", 56)));
+        assertDrawPercentage(7, 97.84, "landCard", cards(cards("anotherCard", 36), cards("landCard", 24)));
     }
 }
